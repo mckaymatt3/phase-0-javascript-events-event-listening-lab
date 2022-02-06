b427009cbd3ae4fdd3398fba95621855b791701b
mckaymatt3 <mckaymatt@outlook.com>
2022-02-06 12:48:10 -0500

    commit tree ea4c334eea37cae7f73d55511d6e52d6dcb55f4d

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function addingEventListener() {const input = document.getElementById('input');
+function addingEventListener() {
+  const input = document.getElementById('input');
 
 function clickAlert() {
   alert('I was clicked!');
@@ -6,3 +7,11 @@ function clickAlert() {
 
 input.addEventListener('click', clickAlert);
 }
+
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
